704c96168eb88e2f4fad4e31f83d433c30e587ab
Rafal Prusak <rafal.prusak@gmail.com>
2018-01-21 07:05:44 -0500

    Dodanie powiazania bilet - uzytkownik.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,9 +3,22 @@ class TicketsController < ApplicationController
 
   def create
     @event = Event.find(params[:event_id])
-    @ticket = @event.tickets.create(ticket_params)
-    @message = "dupa dupa"
-    redirect_to event_path(@event), notice: "You bought new ticket."
+
+    @params = ticket_params
+    @params["event_id"] = params[:event_id]
+    @params["user_id"] = current_user.id
+
+
+    @ticket = Ticket.new(@params)
+    if @ticket.save
+      redirect_to event_path(@event), notice: "You bought new ticket."
+      return
+    else
+      @error_text = ""
+      @ticket.errors.full_messages.each {|m| @error_text += m + ". "}
+      redirect_to event_path(@event), alert: @error_text
+      return
+    end
   end
 
   def ticket_params
--- db/schema.rb
+++ db/schema.rb
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 20180121041403) do
   create_table "tickets", force: :cascade do |t|
     t.integer "places"
     t.integer "event_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_tickets_on_event_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

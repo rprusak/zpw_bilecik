cc9fdca37b2c9bb005e01d5b9331a1de8de056c9
Rafal Prusak <rafal.prusak@gmail.com>
2018-01-22 03:07:55 -0500

    Stworzenie uzytkownika typu administarator.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :correct_user, only: [:create, :destroy]
   def new
 
   end
@@ -21,7 +21,6 @@ class EventsController < ApplicationController
   end
 
   def create
-    puts params
     @event = Event.new(params.require(:event).permit(:name, :description, :price, :event_date, :size, :image,
                                                      :for_children))
 
@@ -30,9 +29,22 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    if !current_user.is_admin
+      redirect_to events_path, alert: "Only administrator can remove events!"
+      return
+    end
+
     @event = Event.find(params[:id])
+    @event.tickets.each {|t| t.destroy}
     @event.destroy
 
     redirect_to events_path
   end
+
+  def correct_user
+    if !current_user.is_admin
+      redirect_to events_path, alert: "Only administrator can perform this action!"
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121134436) do
+ActiveRecord::Schema.define(version: 20180122053238) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180121134436) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "money", default: 0.0
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
